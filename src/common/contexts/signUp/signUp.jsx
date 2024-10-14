99e4f7631196e2e2de7bbcae7575d4517ed619ff
gustavobrayn <gustavo.augusto.brayn@gmail.com>
2024-10-13 23:24:32 -0300

    feat(contexts): create sign up context

---
+++ src/common/contexts/signUp/signUp.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useMemo, useState } from 'react';
+
+const SignUpContext = createContext({});
+
+export const SignUpProvider = (props) => {
+  const { children } = props;
+  const [info, setInfo] = useState({
+    firstName: '',
+    email: '',
+    password: '',
+    color: '',
+    terms: false,
+  });
+
+  const values = useMemo(() => ({ info, setInfo }), [info, setInfo]);
+
+  return (
+    <SignUpContext.Provider value={values}>{children}</SignUpContext.Provider>
+  );
+};
+
+export const useSignUp = () => {
+  const context = useContext(SignUpContext);
+
+  if (!context) {
+    throw new Error('useSignUp must be used within a SignUpProvider');
+  }
+
+  return context;
+};
--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,14 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { SignUpProvider } from './common/contexts/signUp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <SignUpProvider>
         <RouterProvider router={router} />
+      </SignUpProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
